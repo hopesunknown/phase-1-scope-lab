cf04d87b228f2b15fc2575e59f1355ef13243235
Jennifer Tchai <jentchai@gmail.com>
2021-12-15 23:31:37 -0500

    commit tree f374eb6d6ac50def1495a37d2e0bbcb5c7e1d534

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+var bestCustomer
+const leastFavoriteCustomer = 1
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 2;
+}
